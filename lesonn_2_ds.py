192a2dbdde4725695aa01c5c410deef41370e67f
PanoElanus <liuba.isaeva@gmail.com>
2024-09-26 20:01:52 +0300

    Add files via upload

--- lesonn_2_ds.py
+++ lesonn_2_ds.py
@@ -2,7 +2,7 @@ a, b = input("Введите два числа: \n").split()
 
 a, b = map(int, (a, b))
 
-if b !=0:
+if b != 0:
     print(f"{a}/{b}={a/b}")
 else:
     print("Ошибка. На ноль делить нельзя.")
@@ -14,35 +14,51 @@ sum = int(input("Введите стоимость покупки: \n"))
 if sum >= 20:
     sum_sk = round(sum*0.35, 2)
     sum = sum - sum_sk
-
-print(f"Итоговая сумма покупки: {sum_sk} \n Скидка: {sum}")
-
-
-
-#Напишите скрипт, который по введенному пользователем числу от 1 до 12, будет выводить на экран сообщение в виде названия месяца и времени года. Если пользователь введет недопустимое число, программа должна выдавать сообщение об ошибке.
-
-month = int(input(f"Введите номер месяца: /n"))
-
-#for i in range(12):
-
-print("январь - 1")
-print("февраль - 2")
-print("март - 3")
-print("апрель - 4")
-print("май - 5")
-print("июнь - 6")
-print("июль - 7")
-print("август - 8")
-print("сентябрь - 9")
-print("октябрь - 10")
-print("ноябрь - 11")
-print("декабрь - 12")
-
-if month//3 == 1:
-    season = "весна"
-elif month//3 == 2:
-    season = "лето"
-elif month//3 == 3:
-    season = "осень"
 else:
-    season = "зима"
+    sum_sk = 0
+
+print(f"Итоговая сумма покупки: {sum} \n Скидка: {sum_sk}")
+
+
+
+month = input(f"Введите номер месяца: \n")
+
+if not(month.isdigit()) or int(month) > 12:
+    print("Введите нормальное значение")
+    
+else:
+    month = int(month)
+    if month // 3 == 1:
+        season = "весна"
+        if month == 3:
+            month = "март"
+        elif month == 4:
+            month = "апрель"
+        else:
+            month = "май"
+    elif month//3 == 2:
+        season = "лето"
+        if month == 6:
+            month = "июнь"
+        elif month == 7:
+            month = "июль"
+        else:
+            month = "август"
+    elif month//3 == 3:
+        season = "осень"
+        if month == 9:
+            month = "сентябрь"
+        elif month == 10:
+            month = "октябрь"
+        else:
+            month = "ноябрь"
+    else:
+        season = "зима"
+        if month == 12:
+            month = "декабрь"
+        elif month == 1:
+            month = "январь"
+        else:
+            month = "февраль"
+
+    print(f"Месяц - {month}, сезон - {season}")
